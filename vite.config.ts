d3e9c9145862a2c2624545f02e0b2bf25a1d61e2
s6802061811096 <s6802061811096@email.kmutnb.ac.th>
2026-09-18 22:28:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/-/',
+  base: '/smart-ot-teaching/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
